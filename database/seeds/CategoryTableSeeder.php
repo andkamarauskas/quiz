c8997a0fe282cdd148199d934f1a49d7eb400540
Andrius Kamarauskas <andkamarauskas@gmail.com>
2017-09-28 15:24:30 +0300

    laravel project started, added crud generator, created category crud

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+    		'Filmai',
+    		'Sportas',
+    		'Mokslas',
+    		'Istorija',
+    		'Geografija',
+    		'Knygos',
+    		'Muzika'
+    		];
+
+    	foreach ($categories as $category) {
+    		DB::table('categories')->insert([
+    			'title'=>$category
+    			]);
+    	}
+    }
+}
